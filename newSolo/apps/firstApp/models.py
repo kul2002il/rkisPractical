f5f8892f91f77c4f3de9dd51f38618c12fdf35f1
Ilia <kul.2002.il@gmail.com>
2020-10-12 18:28:52 +0700

    Удаление firstApp

--- newSolo/apps/firstApp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-	fio = models.CharField(max_length=50, verbose_name='ФИО', blank=True)
