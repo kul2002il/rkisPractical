c6fd62ff2060b4c1346e568d4536cee2f23c3534
Ilia <kul.2002.il@gmail.com>
2020-10-12 18:17:19 +0700

    Добавлена модель User

--- newSolo/apps/firstApp/models.py
+++ newSolo/apps/firstApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+	fio = models.CharField(max_length=50, verbose_name='ФИО', blank=True)
