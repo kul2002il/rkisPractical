22ed9e36c57b6bf01a82042b004d38724358c2af
Ilia <kul.2002.il@gmail.com>
2020-10-13 19:32:28 +0700

    Добавлена модель Специальность.

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Specialty
+
 
 admin.site.register(User)
+admin.site.register(Specialty)
 
--- myApp/models.py
+++ myApp/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class Specialty(models.Model):
+    nameRol = models.CharField(max_length=50, verbose_name="name", default='')
+
+
 class User(AbstractUser):
     fio = models.CharField(max_length=50, verbose_name='ФИО', blank=True)
 
