efc4f54764f11a2a211f932bd4b82eee43cfa781
Ilia <kul.2002.il@gmail.com>
2020-10-14 10:42:39 +0700

    Cозданы страницы входа, профиля, выхода.

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from .views import BBLoginView, BBLogoutView
+
+app_name = 'myApp'
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('accounts/login/', BBLoginView.as_view(), name="login"),
+    path('accounts/profile/', views.profile, name="profile"),
+    path('accounts/logout/', BBLogoutView.as_view(), name="logout"),
 ]
 
--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
-def index(refery):
-	return HttpResponse("Pfgecr.")
+
+def index(request):
+	return render(request, 'main/index.html')
+
+
+class BBLoginView(LoginView):
+	template_name = 'main/login.html'
+
+
+@login_required
+def profile(request):
+	return render(request, 'main/profile.html')
+
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+	template_name = 'main/logout.html'
