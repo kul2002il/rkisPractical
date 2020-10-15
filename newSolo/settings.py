1c4f0172f304cb87e346139ad16438fddf0aa938
Ilia <kul.2002.il@gmail.com>
2020-10-15 17:47:17 +0700

    Почти работает регистрация.

--- newSolo/settings.py
+++ newSolo/settings.py
@@ -123,8 +123,8 @@ STATIC_URL = '/static/'
 
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'username@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+# EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'kul.2002.il@mail.ru'
+EMAIL_HOST_PASSWORD = 'esperantonumber1'
 EMAIL_PORT = 587
 
